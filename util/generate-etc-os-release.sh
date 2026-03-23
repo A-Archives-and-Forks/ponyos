793d0b83aac3abbb4f44cb164fd26a41981a2c35
K. Lange <klange@toaruos.org>
2026-03-23 21:33:29 +0900

    base: ponify /etc/issue, /etc/os-release

--- util/generate-etc-os-release.sh
+++ util/generate-etc-os-release.sh
@@ -5,12 +5,12 @@ MINOR=$(grep __kernel_version_minor kernel/sys/version.c | sed s'/.*= \(.*\);/\1
 LOWER=$(grep __kernel_version_lower kernel/sys/version.c | sed s'/.*= \(.*\);/\1/')
 
 cat << EOF
-PRETTY_NAME="ToaruOS ${MAJOR}.${MINOR}"
-NAME="ToaruOS"
+PRETTY_NAME="PonyOS ${MAJOR}.${MINOR}"
+NAME="PonyOS"
 VERSION_ID="${MAJOR}.${MINOR}.${LOWER}"
 VERSION="${MAJOR}.${MINOR}.${LOWER}"
-ID=toaru
-HOME_URL="https://toaruos.org/"
-SUPPORT_URL="https://github.com/klange/toaruos"
-BUG_REPORT_URL="https://github.com/klange/toaruos"
+ID=ponyos
+HOME_URL="https://ponyos.org/"
+SUPPORT_URL="https://github.com/klange/ponyos"
+BUG_REPORT_URL="https://github.com/klange/ponyos"
 EOF
